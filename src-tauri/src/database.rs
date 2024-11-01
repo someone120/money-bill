6f4e072d0dbd02a6433d766b6e48e4a9a2d0db9a
someone120 <mouren1101@outlook.com>
2024-11-01 20:49:38 +0800

    chore: move database operate out lib.rs

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,4 +1,89 @@
+use details::Details;
+use rusqlite::params;
+use rusqlite::Connection;
+use rust_decimal::Decimal;
+use rust_decimal::prelude::*;
+use rust_decimal_macros::dec;
 pub mod account;
 pub mod details;
+use error::Error;
+
+use crate::error;
 pub mod init;
 pub mod transaction;
+
+pub fn get_incomed(conn: &Connection) -> Result<Decimal, Error> {
+    let mut result = Decimal::zero();
+    let mut stmt = conn
+        .prepare("SELECT DETAIL.id,trans_id,account,currency,balance FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y-%m', time) = strftime('%Y-%m', 'now')")?;
+    let iter = stmt.query_map(params![], |row| {
+        Ok(Details {
+            id: row.get(0)?,
+            trans_id: row.get(1)?,
+            account: row.get(2)?,
+            currency: row.get(3)?,
+            balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
+        })
+    })?;
+    for i in iter {
+        let details = i.unwrap();
+        if details.account.starts_with("income::") {
+            result += details.balance;
+        }
+    }
+
+    Ok(result)
+}
+
+pub fn get_expenses(conn: &Connection) -> Result<Decimal, Error> {
+    let mut result = Decimal::zero();
+    let mut stmt = conn
+        .prepare("SELECT DETAIL.id,trans_id,account,currency,balance FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y-%m', time) = strftime('%Y-%m', 'now')")?;
+    let iter = stmt.query_map(params![], |row| {
+        Ok(Details {
+            id: row.get(0)?,
+            trans_id: row.get(1)?,
+            account: row.get(2)?,
+            currency: row.get(3)?,
+            balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
+        })
+    })?;
+    for i in iter {
+        let details = i.unwrap();
+        if details.account.starts_with("expenses::") {
+            result += details.balance;
+        }
+    }
+
+    Ok(result)
+}
+
+pub fn recalcute(conn: &Connection) -> Result<(), Error> {
+    let accounts = account::get_accounts(conn)?;
+    for account in accounts {
+        let details = details::get_details_by_account(conn, account.name.as_str())?;
+        let mut balance = dec!(0.0);
+        for i in details {
+            balance += i.balance;
+        }
+        let mut acc = account;
+        acc.balance = balance;
+        account::update_account(conn, &acc)?;
+    }
+    Ok(())
+}
+
+// fn verification(conn: &Connection) -> Result<(), Error> {
+//     let trans = transaction::get_transactions(conn)?;
+//     for tran in trans {
+//         let details = details::get_details_by_trans(conn, tran.id.as_str())?;
+//         let mut balance = dec!(0.0);
+//         for i in details {
+//             balance += i.balance;
+//         }
+//         if balance != Decimal::zero() {
+//             return Err(Error::DetailSumNotZero(tran.id, balance.to_f32().unwrap()));
+//         }
+//     }
+//     Ok(())
+// }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,7 @@
 use std::{fs, sync::Mutex};
 
 use database::{
-    account::{self},
-    details::{self, Details},
-    init,
-    transaction::Transaction,
+    account::{self}, details::{self, Details}, get_expenses, get_incomed, init, transaction::{self, Transaction}
 };
 use error::Error;
 use rusqlite::{params, Connection};
@@ -15,101 +12,6 @@ mod error;
 
 struct ConnectionWrapper(pub Mutex<Connection>);
 
-fn get_incomed(conn: &Connection) -> Result<Decimal, Error> {
-    let mut result = Decimal::zero();
-    let mut stmt = conn
-        .prepare("SELECT * FROM TRANS WHERE strftime('%Y-%m', time) = strftime('%Y-%m', 'now');")?;
-    let iter = stmt.query_map(params![], |row| {
-        Ok(Transaction {
-            id: row.get(0)?,
-            date: row.get(1)?,
-            extra: row.get(2)?,
-        })
-    })?;
-    for i in iter {
-        let mut stmt = conn.prepare("SELECT * FROM DETAIL WHERE trans_id=?1")?;
-        let iter = stmt.query_map(params![i.unwrap().id], |row| {
-            Ok(Details {
-                id: row.get(0)?,
-                trans_id: row.get(1)?,
-                account: row.get(2)?,
-                currency: row.get(3)?,
-                balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
-            })
-        })?;
-        for i in iter {
-            let details = i.unwrap();
-            if details.account.starts_with("income::"){
-                result += details.balance;
-            }
-        }
-    }
-
-    Ok(result)
-}
-
-fn get_expenses(conn: &Connection) -> Result<Decimal, Error> {
-    let mut result = Decimal::zero();
-    let mut stmt = conn
-        .prepare("SELECT * FROM TRANS WHERE strftime('%Y-%m', time) = strftime('%Y-%m', 'now');")?;
-    let iter = stmt.query_map(params![], |row| {
-        Ok(Transaction {
-            id: row.get(0)?,
-            date: row.get(1)?,
-            extra: row.get(2)?,
-        })
-    })?;
-    for i in iter {
-        let mut stmt = conn.prepare("SELECT * FROM DETAIL WHERE trans_id=?1")?;
-        let iter = stmt.query_map(params![i.unwrap().id], |row| {
-            Ok(Details {
-                id: row.get(0)?,
-                trans_id: row.get(1)?,
-                account: row.get(2)?,
-                currency: row.get(3)?,
-                balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
-            })
-        })?;
-        for i in iter {
-            let details = i.unwrap();
-            if details.account.starts_with("expenses::"){
-                result += details.balance;
-            }
-        }
-    }
-
-    Ok(result)
-}
-
-fn recalcute(conn: &Connection) -> Result<(), Error> {
-    let accounts = account::get_accounts(conn)?;
-    for account in accounts {
-        let details = details::get_details_by_account(conn, account.name.as_str())?;
-        let mut balance = dec!(0.0);
-        for i in details {
-            balance += i.balance;
-        }
-        let mut acc = account;
-        acc.balance = balance;
-        account::update_account(conn, &acc)?;
-    }
-    Ok(())
-}
-
-// fn verification(conn: &Connection) -> Result<(), Error> {
-//     let trans = transaction::get_transactions(conn)?;
-//     for tran in trans {
-//         let details = details::get_details_by_trans(conn, tran.id.as_str())?;
-//         let mut balance = dec!(0.0);
-//         for i in details {
-//             balance += i.balance;
-//         }
-//         if balance != Decimal::zero() {
-//             return Err(Error::DetailSumNotZero(tran.id, balance.to_f32().unwrap()));
-//         }
-//     }
-//     Ok(())
-// }
 
 #[tauri::command]
 fn get_income_expenses(conn: tauri::State<ConnectionWrapper>) -> Vec<f32> {
@@ -122,18 +24,18 @@ fn get_income_expenses(conn: tauri::State<ConnectionWrapper>) -> Vec<f32> {
     // details::add_details(
     //     &conn,
     //     id.as_str(),
-    //     bar_id.as_str(),
+    //     "income::a",
     //     "USD",
     //     Decimal::from_f32_retain(12.0).unwrap(),
     // );
     // details::add_details(
     //     &conn,
     //     id.as_str(),
-    //     foo_id.as_str(),
+    //     "expenses::a",
     //     "USD",
     //     Decimal::from_f32_retain(-12.0).unwrap(),
     // );
-    recalcute(&conn).unwrap();
+    // recalcute(&conn).unwrap();
     let result = vec![
         get_incomed(&conn).unwrap().to_f32().unwrap() * -1.0,
         get_expenses(&conn).unwrap().to_f32().unwrap(),
@@ -177,6 +79,8 @@ pub fn run() {
 
 #[cfg(test)]
 mod tests {
+    use database::recalcute;
+
     use crate::database::{init, transaction};
 
     use super::*;
