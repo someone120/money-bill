a997fba4305dd13f255775887fb37be440b84f15
someone120 <mouren1101@outlook.com>
2024-11-16 22:27:24 +0800

    docs: add more comment

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -13,10 +13,21 @@ use crate::error;
 pub mod init;
 pub mod transaction;
 
+/// Calculates the total income for the current month.
+///
+/// # Arguments
+///
+/// * `conn` - A connection to the database.
+///
+/// # Returns
+///
+/// * The total income as a `Decimal`.
 pub fn get_month_incomed(conn: &Connection) -> Result<Decimal, Error> {
-    let mut result = Decimal::zero();
+  let mut result = Decimal::zero();
+    // Prepare and execute SQL statement to select relevant transactions for the current month.
     let mut stmt = conn
         .prepare("SELECT DETAIL.id,trans_id,account,currency,balance FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y-%m', time) = strftime('%Y-%m', 'now')")?;
+    // Iterate over the results to calculate total income.
     let iter = stmt.query_map(params![], |row| {
         Ok(Details {
             id: row.get(0)?,
@@ -28,6 +39,7 @@ pub fn get_month_incomed(conn: &Connection) -> Result<Decimal, Error> {
     })?;
     for i in iter {
         let details = i.unwrap();
+        // If the transaction is related to income, add its balance to the total.
         if details.account.starts_with("income::") {
             result += details.balance;
         }
@@ -36,10 +48,21 @@ pub fn get_month_incomed(conn: &Connection) -> Result<Decimal, Error> {
     Ok(result)
 }
 
+/// Calculates the total expenses for the current month.
+///
+/// # Arguments
+///
+/// * `conn` - A connection to the database.
+///
+/// # Returns
+///
+/// * The total expenses as a `Decimal`.
 pub fn get_month_expenses(conn: &Connection) -> Result<Decimal, Error> {
     let mut result = Decimal::zero();
+    // Prepare and execute SQL statement to select relevant transactions for the current month.
     let mut stmt = conn
         .prepare("SELECT DETAIL.id,trans_id,account,currency,balance FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y-%m', time) = strftime('%Y-%m', 'now')")?;
+    // Iterate over the results to calculate total expenses.
     let iter = stmt.query_map(params![], |row| {
         Ok(Details {
             id: row.get(0)?,
@@ -51,6 +74,7 @@ pub fn get_month_expenses(conn: &Connection) -> Result<Decimal, Error> {
     })?;
     for i in iter {
         let details = i.unwrap();
+        // If the transaction is related to expenses, add its balance to the total.
         if details.account.starts_with("expenses::") {
             result += details.balance;
         }
@@ -59,14 +83,26 @@ pub fn get_month_expenses(conn: &Connection) -> Result<Decimal, Error> {
     Ok(result)
 }
 
+/// Recalculates account balances and updates them in the database.
+///
+/// # Arguments
+///
+/// * `conn` - A connection to the database.
+///
+/// # Returns
+///
+/// * An `Result<()>` indicating success or failure of the operation.
 pub fn recalcute(conn: &Connection) -> Result<(), Error> {
+    // Get all accounts from the database.
     let accounts = account::get_accounts(conn)?;
     for account in accounts {
+        // Get details for each account and calculate the total balance.
         let details = details::get_details_by_account(conn, account.name.as_str())?;
         let mut balance = dec!(0.0);
         for i in details {
             balance += i.balance;
         }
+        // Update the account's balance with the calculated value.
         let mut acc = account;
         acc.balance = balance;
         account::update_account(conn, &acc)?;
@@ -74,52 +110,76 @@ pub fn recalcute(conn: &Connection) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn get_weekly_expenses(conn: &Connection)-> Result<Vec<f32>,Error>{
-    let mut result = vec![0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+/// Calculates weekly expenses for the current week.
+///
+/// # Arguments
+///
+/// * `conn` - A connection to the database.
+///
+/// # Returns
+///
+/// * A vector of f32 representing daily expenses from Monday to Sunday.
+pub fn get_weekly_expenses(conn: &Connection) -> Result<Vec<f32>, Error> {
+    let mut result = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    // Prepare and execute SQL statement to select relevant transactions for the current week.
     let mut stmt = conn
         .prepare("SELECT DETAIL.id,trans_id,account,currency,balance,time FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y %W', time) = strftime('%Y %W', 'now') ORDER BY TIME")?;
-    let iter = stmt.query_map(params![], |row: &rusqlite::Row<'_>|->rusqlite::Result<(Details,String)>{
+    // Iterate over the results to calculate weekly expenses.
+    let iter = stmt.query_map(params![], |row: &rusqlite::Row<'_>| -> rusqlite::Result<(Details, String)> {
         Ok((Details {
             id: row.get(0)?,
             trans_id: row.get(1)?,
             account: row.get(2)?,
             currency: row.get(3)?,
             balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
-            
-        },row.get(5)?))
+        }, row.get(5)?))
     })?;
     for i in iter {
         let s = i?;
+        // Skip income transactions.
         if s.0.account.starts_with("income") {
             continue;
         }
-        let date=chrono::NaiveDateTime::parse_from_str(s.1.as_str(),&"%Y-%m-%d %H:%M".to_string()).unwrap();
+        // Parse the date and add the balance to the corresponding day of the week.
+        let date = chrono::NaiveDateTime::parse_from_str(s.1.as_str(), "%Y-%m-%d %H:%M").unwrap();
         result[date.weekday().num_days_from_monday() as usize] += s.0.balance.to_f32().unwrap();
     }
 
     Ok(result)
 }
 
-pub fn get_weekly_income(conn: &Connection)-> Result<Vec<f32>,Error>{
-    let mut result = vec![0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+/// Calculates weekly income for the current week.
+///
+/// # Arguments
+///
+/// * `conn` - A connection to the database.
+///
+/// # Returns
+///
+/// * A vector of f32 representing daily income from Monday to Sunday.
+pub fn get_weekly_income(conn: &Connection) -> Result<Vec<f32>, Error> {
+    let mut result = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+    // Prepare and execute SQL statement to select relevant transactions for the current week.
     let mut stmt = conn
         .prepare("SELECT DETAIL.id,trans_id,account,currency,balance,time FROM DETAIL INNER JOIN TRANS ON trans_id=TRANS.id and strftime('%Y %W', time) = strftime('%Y %W', 'now') ORDER BY TIME")?;
-    let iter = stmt.query_map(params![], |row: &rusqlite::Row<'_>|->rusqlite::Result<(Details,String)>{
+    // Iterate over the results to calculate weekly income.
+    let iter = stmt.query_map(params![], |row: &rusqlite::Row<'_>| -> rusqlite::Result<(Details, String)> {
         Ok((Details {
             id: row.get(0)?,
             trans_id: row.get(1)?,
             account: row.get(2)?,
             currency: row.get(3)?,
             balance: Decimal::from_f32_retain(row.get::<usize, f32>(4)?).unwrap(),
-            
-        },row.get(5)?))
+        }, row.get(5)?))
     })?;
     for i in iter {
         let s = i?;
+        // Skip expenses transactions.
         if s.0.account.starts_with("expenses") {
             continue;
         }
-        let date=chrono::NaiveDateTime::parse_from_str(s.1.as_str(),"%Y-%m-%d %H:%M").unwrap();
+        // Parse the date and add the balance to the corresponding day of the week.
+        let date = chrono::NaiveDateTime::parse_from_str(s.1.as_str(), "%Y-%m-%d %H:%M").unwrap();
         result[date.weekday().num_days_from_monday() as usize] += s.0.balance.to_f32().unwrap();
     }
 
@@ -151,8 +211,8 @@ mod tests {
     fn test_recalcute() -> Result<(), Error> {
         let conn = Connection::open_in_memory()?;
         init::init(&conn)?;
-        account::add_account(&conn, "income::bar", "USD")?;
-        account::add_account(&conn, "expenses::foo", "USD")?;
+        account::add_account(&conn, "income::bar", "USD","","")?;
+        account::add_account(&conn, "expenses::foo", "USD","","")?;
 
         let date = chrono::Utc::now();
         let id = transaction::add_transaction(&conn, date, "")?;
--- src-tauri/src/database/account.rs
+++ src-tauri/src/database/account.rs
@@ -1,52 +1,102 @@
 use rusqlite::{params, Connection, Result};
 use rust_decimal::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
 pub struct Account {
     pub name: String,
     pub balance: Decimal,
     pub currency: String,
+    pub icon: Option<String>,
+    pub extra: Option<String>,
 }
 
-pub fn add_account(conn: &Connection, name: &str, currency: &str) -> Result<()> {
+// Function to add a new account to the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+/// * `name` - The name of the account as a string slice.
+/// * `currency` - The currency of the account as a string slice.
+/// * `icon` - An optional icon for the account as a string slice.
+/// * `extra` - Any additional information about the account as a string slice.
+///
+/// # Returns
+/// * `Result<()>` - On success, returns Ok(()). On failure, returns an error.
+pub fn add_account(
+  conn: &Connection,
+    name: &str,
+    currency: &str,
+    icon: &str,
+    extra: &str,
+) -> Result<()> {
+    // SQLite query to insert a new account into the database
     conn.execute(
-        "INSERT INTO ACCOUNT (name,currency,balance) VALUES (?1,?2,?3)",
-        (name, currency, 0.0),
+        "INSERT INTO ACCOUNT (name,currency,balance,icon,extra ) VALUES (?1,?2,?3,?4,?5)",
+        params![name, currency, 0.0, icon, extra],
     )?;
     Ok(())
 }
-
-/// Get info of account
+/// Function to retrieve account information from the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+/// * `name` - The name of the account as a string slice.
+///
+/// # Returns
+/// * `Result<Vec<Account>>` - On success, returns a vector of Account structs. On failure, returns an error.
 pub fn read_account(conn: &Connection, name: &str) -> Result<Vec<Account>> {
+  // Prepare and execute the SQL query to select account information by name
     let mut stmt = conn.prepare("SELECT * FROM ACCOUNT WHERE name = ?")?;
+
+    // Map each row from the result set into an Account struct
     let iter = stmt.query_map(params![name], |row| {
         Ok(Account {
             name: row.get(0)?,
             currency: row.get(1)?,
             balance: Decimal::from_f32_retain(row.get::<usize, f32>(2)?).unwrap(),
+            icon: row.get(3)?,
+            extra: row.get(4)?,
         })
     })?;
+
+    // Collect the results into a vector
     let mut result = Vec::new();
     for i in iter {
         result.push(i?)
     }
     Ok(result)
 }
-
-/// Remove account
+/// Function to remove an account from the database by name
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+/// * `name` - The name of the account as a string slice.
+///
+/// # Returns
+/// * `Result<()>` - On success, returns Ok(()). On failure, returns an error.
 pub fn del_account(conn: &Connection, name: &str) -> Result<()> {
+  // SQLite query to delete an account from the database by name
     conn.execute("DELETE FROM ACCOUNT WHERE name=?1", params![name])?;
     Ok(())
 }
 
-/// Get all account
+/// Function to get all accounts from the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+///
+/// # Returns
+/// * `Result<Vec<Account>>` - On success, returns a vector of Account structs. On failure, returns an error.
 pub fn get_accounts(conn: &Connection) -> Result<Vec<Account>> {
+    // Prepare and execute the SQL query to select all accounts from the database
     let mut stmt = conn.prepare("SELECT * FROM ACCOUNT")?;
     let iter = stmt.query_map([], |row| {
         Ok(Account {
             name: row.get(0)?,
             currency: row.get(1)?,
             balance: Decimal::from_f32_retain(row.get::<usize, f32>(2)?).unwrap(),
+            icon: row.get(3)?,
+            extra: row.get(4)?,
         })
     })?;
     let mut result = Vec::new();
@@ -56,18 +106,80 @@ pub fn get_accounts(conn: &Connection) -> Result<Vec<Account>> {
     Ok(result)
 }
 
+/// Function to get all income accounts from the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+///
+/// # Returns
+/// * `Result<Vec<Account>>` - On success, returns a vector of Account structs. On failure, returns an error.
+pub fn get_income_accounts(conn: &Connection) -> Result<Vec<Account>> {
+    // Prepare and execute the SQL query to select all accounts with names starting with 'income::' from the database
+    let mut stmt = conn.prepare("SELECT * FROM ACCOUNT WHERE name LIKE 'income::%'")?;
+    let iter = stmt.query_map([], |row| {
+        Ok(Account {
+            name: row.get(0)?,
+            currency: row.get(1)?,
+            balance: Decimal::from_f32_retain(row.get::<usize, f32>(2)?).unwrap(),
+            icon: row.get(3)?,
+            extra: row.get(4)?,
+        })
+    })?;
+    let mut result = Vec::new();
+    for i in iter {
+        result.push(i?)
+    }
+    Ok(result)
+}
+
+/// Function to get all expense accounts from the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+///
+/// # Returns
+/// * `Result<Vec<Account>>` - On success, returns a vector of Account structs. On failure, returns an error.
+pub fn get_expenses_accounts(conn: &Connection) -> Result<Vec<Account>> {
+    // Prepare and execute the SQL query to select all accounts with names starting with 'expenses::' from the database
+    let mut stmt = conn.prepare("SELECT * FROM ACCOUNT WHERE name LIKE 'expenses::%'")?;
+    let iter = stmt.query_map([], |row| {
+        Ok(Account {
+            name: row.get(0)?,
+            currency: row.get(1)?,
+            balance: Decimal::from_f32_retain(row.get::<usize, f32>(2)?).unwrap(),
+            icon: row.get(3)?,
+            extra: row.get(4)?,
+        })
+    })?;
+    let mut result = Vec::new();
+    for i in iter {
+        result.push(i?)
+    }
+    Ok(result)
+}
+
+/// Function to update an account in the database
+///
+/// # Arguments
+/// * `conn` - A reference to the SQLite connection object.
+/// * `account` - An Account struct containing updated information.
+///
+/// # Returns
+/// * `Result<()>` - On success, returns Ok(()). On failure, returns an error.
 pub fn update_account(conn: &Connection, account: &Account) -> Result<()> {
+    // SQLite query to update the specified account in the database with new values
     conn.execute(
-        "UPDATE ACCOUNT SET currency=?2,balance=?3 WHERE name=?1",
+        "UPDATE ACCOUNT SET currency=?2,balance=?3,icon=?4,extra=?5 WHERE name=?1",
         (
             account.name.clone(),
             account.currency.clone(),
             account.balance.to_f32(),
+            account.icon.clone(),
+            account.extra.clone(),
         ),
     )?;
     Ok(())
 }
-
 #[cfg(test)]
 mod tests {
     use crate::database::init::init;
@@ -77,14 +189,16 @@ mod tests {
     fn test_add_account() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
-        add_account(&conn, "someone", "CNY")?;
+        add_account(&conn, "someone", "CNY", "", "")?;
 
-        let mut stmt = conn.prepare("SELECT name,currency,balance FROM ACCOUNT")?;
+        let mut stmt = conn.prepare("SELECT name,currency,balance,icon,extra FROM ACCOUNT")?;
         let mut iter = stmt.query_map([], |row| {
             Ok(Account {
                 name: row.get(0)?,
                 currency: row.get(1)?,
                 balance: Decimal::from_f32_retain(row.get::<usize, f32>(2)?).unwrap(),
+                icon: row.get(3)?,
+                extra: row.get(4)?,
             })
         })?;
         if iter.next().is_some() {
@@ -96,7 +210,7 @@ mod tests {
     fn test_read_account() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
-        add_account(&conn, "a", "b")?;
+        add_account(&conn, "a", "b", "", "")?;
         let accounts = get_accounts(&conn)?;
         let a = read_account(&conn, &accounts[0].name)?;
         println!("{:?}", a);
@@ -107,7 +221,7 @@ mod tests {
     fn test_get_last_id() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
-        add_account(&conn, "a", "b")?;
+        add_account(&conn, "a", "b", "", "")?;
 
         let accounts = get_accounts(&conn)?;
 
@@ -118,7 +232,7 @@ mod tests {
     fn test_delete() -> Result<()> {
         let conn = Connection::open_in_memory()?;
         init(&conn)?;
-        add_account(&conn, "a", "b")?;
+        add_account(&conn, "a", "b", "", "")?;
         let id = get_accounts(&conn)?;
         del_account(&conn, &id[0].name)?;
 
