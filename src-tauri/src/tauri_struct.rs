11b64395ee170fbfec143b350a717d8e26d8a252
someone120 <mouren1101@outlook.com>
2025-08-07 23:04:07 +0800

    chore: Format Rust struct definitions for better readability

--- src-tauri/src/tauri_struct.rs
+++ src-tauri/src/tauri_struct.rs
@@ -1,20 +1,19 @@
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Serialize, Deserialize)]
-pub struct WeeklyIncomeExpenses{
-    pub income:Vec<f32>,
-    pub expenses:Vec<f32>
+pub struct WeeklyIncomeExpenses {
+    pub income: Vec<f32>,
+    pub expenses: Vec<f32>,
 }
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct AccountIconName {
     pub name:String,
     pub icon:String,
     pub currency:String
 }
 
-#[derive(Serialize,Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct AccountAmount {
-    pub account:String,
-    pub amount:f32
+    pub account: String,
+    pub amount: f32,
 }
